1595c0b65ad1159759319964e2787eef8e2d59fa
Bri Grier <brianna.g6879@gmail.com>
2024-04-25 17:33:00 -0500

    GET for orders

--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
+const controller = require("../orders/orders.controller")
 
-// TODO: Implement the /orders routes needed to make the tests pass
+router.route("/").get(controller.list)
 
 module.exports = router;
