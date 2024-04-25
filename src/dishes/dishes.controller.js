addd9ce6adcd02782da1077e511a10c08505a5e8
Bri Grier <brianna.g6879@gmail.com>
2024-04-25 14:25:23 -0500

    GET func to get alll dishes

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -1,9 +1,19 @@
 const path = require("path");
 
+
 // Use the existing dishes data
 const dishes = require(path.resolve("src/data/dishes-data"));
 
 // Use this function to assign ID's when necessary
 const nextId = require("../utils/nextId");
 
-// TODO: Implement the /dishes handlers needed to make the tests pass
+//GET
+function list(req, res){
+    res.json({data:dishes})
+}
+
+
+
+module.exports = {
+    list,
+}
--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
+const controller = require("../dishes/dishes.controller")
 
-// TODO: Implement the /dishes routes needed to make the tests pass
+router.route("/").get(controller.list)
 
 module.exports = router;
