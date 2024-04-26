37661663bd54c83bc1d2ffacc1a39b71d26f489e
Bri Grier <brianna.g6879@gmail.com>
2024-04-26 11:37:12 -0500

    error handler in dishes.router

--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const controller = require("../dishes/dishes.controller")
+const methodNotAllowed = require("../errors/methodNotAllowed")
 
-router.route("/").get(controller.list).post(controller.create)
-router.route("/:dishId").get(controller.read).put(controller.update).delete(controller.destroy)
+router.route("/").get(controller.list).post(controller.create).all(methodNotAllowed)
+router.route("/:dishId").get(controller.read).put(controller.update).delete(controller.destroy).all(methodNotAllowed)
 
 module.exports = router;
